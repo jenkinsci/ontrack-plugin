98394d051d8c957b3e9cb670e6b76aa00b35b9ee
flesire <fabien.lesire@gmail.com>
2014-09-11 10:19:17 +0200

    Get last build with a promotion level action works now.

--- src/main/java/net/nemerosa/ontrack/jenkins/OntrackLastBuildWithPromotionLevel.java
+++ src/main/java/net/nemerosa/ontrack/jenkins/OntrackLastBuildWithPromotionLevel.java
@@ -6,9 +6,13 @@ import hudson.Launcher;
 import hudson.model.*;
 import hudson.tasks.BuildStepDescriptor;
 import hudson.tasks.Builder;
+import net.nemerosa.ontrack.jenkins.support.client.OntrackClient;
 import org.kohsuke.stapler.DataBoundConstructor;
 
 import java.io.IOException;
+import java.io.PrintStream;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import static net.nemerosa.ontrack.jenkins.support.client.OntrackClient.forBranch;
 
@@ -51,9 +55,9 @@ public class OntrackLastBuildWithPromotionLevel extends Builder {
         final String actualBranch = OntrackPluginSupport.expand(branch, theBuild, listener);
         final String actualPromotionLevel = OntrackPluginSupport.expand(promotionLevel, theBuild, listener);
 
-        JsonNode lastBuild =forBranch(listener.getLogger(), actualProject, actualBranch).on("_status").get();
+        JsonNode lastBuild = getBuild(System.out, actualProject, actualBranch, actualPromotionLevel);
         if (lastBuild != null) {
-            String name = lastBuild.path("name").asText();
+            String name = lastBuild.path("name").textValue();
             listener.getLogger().format("Found build '%s' for branch '%s' and project '%s' and promotion level '%s'%n", name, actualBranch, actualProject, actualPromotionLevel);
             theBuild.addAction(new ParametersAction(new StringParameterValue(variable, name)));
         } else {
@@ -61,6 +65,31 @@ public class OntrackLastBuildWithPromotionLevel extends Builder {
             theBuild.setResult(Result.FAILURE);
         }
         return true;
+    }
+
+    /**
+     * Get last build with a certain promotion level on a branch
+     * @param logger The logger
+     * @param project The project
+     * @param branch The branch
+     * @param promotionLevel The promotion level
+     * @return The build or null of no build found.
+     * @throws IOException
+     */
+    private JsonNode getBuild(PrintStream logger, final String project, final String branch, final String promotionLevel) throws IOException {
+        Map<String, Object> filter = new LinkedHashMap<String, Object>();
+        filter.put("count", 1);
+        filter.put("withPromotionLevel", promotionLevel);
+        JsonNode branchBuildView = OntrackClient.forBranch(logger, project, branch)
+                .on("_view", "/net.nemerosa.ontrack.service.StandardBuildFilterProvider", filter)
+                .get();
+        JsonNode buildViews = branchBuildView.get("buildViews");
+        if (buildViews.size() > 0) {
+            // Gets the first build that complies
+            return buildViews.get(0).get("build");
+        } else {
+            return null;
+        }
     }
 
     @Extension
