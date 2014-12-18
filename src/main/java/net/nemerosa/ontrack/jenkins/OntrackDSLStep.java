e8d807ae8e3f100cd0d0168e5c1df1dbf699d893
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-12-18 21:03:32 +0100

    Evaluation of script

--- src/main/java/net/nemerosa/ontrack/jenkins/OntrackDSLStep.java
+++ src/main/java/net/nemerosa/ontrack/jenkins/OntrackDSLStep.java
@@ -1,15 +1,24 @@
 package net.nemerosa.ontrack.jenkins;
 
+import groovy.lang.Binding;
+import groovy.lang.GroovyShell;
 import hudson.Extension;
 import hudson.Launcher;
 import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
 import hudson.model.BuildListener;
+import hudson.model.Result;
 import hudson.tasks.BuildStepDescriptor;
 import hudson.tasks.Builder;
+import net.nemerosa.ontrack.dsl.Ontrack;
+import net.nemerosa.ontrack.dsl.OntrackConnection;
+import net.nemerosa.ontrack.jenkins.support.JenkinsConnector;
+import org.apache.commons.lang.StringUtils;
 import org.kohsuke.stapler.DataBoundConstructor;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Step that allows to call the Ontrack DSL.
@@ -41,7 +50,55 @@ public class OntrackDSLStep extends Builder {
 
     @Override
     public boolean perform(AbstractBuild<?, ?> theBuild, Launcher launcher, BuildListener listener) throws InterruptedException, IOException {
+        // Connection to Ontrack
+        Ontrack ontrack = createOntrackConnector();
+        // Connector to Jenkins
+        JenkinsConnector jenkins = new JenkinsConnector();
+        // Values to bind
+        Map<String, Object> values = new HashMap<String, Object>();
+        // TODO Gets the environment
+        // TODO Gets the properties
+        // Binding
+        values.put("ontrack", ontrack);
+        values.put("jenkins", jenkins);
+        Binding binding = new Binding(values);
+        // Groovy shell
+        GroovyShell shell = new GroovyShell(binding);
+        // TODO Groovy sandbox in Jenkins?
+        // Runs the script
+        Object shellResult = shell.evaluate(script);
+        listener.getLogger().format("Ontrack DSL script returned result: %s", shellResult);
+        // Result
+        Result result = toJenkinsResult(shellResult);
+        listener.getLogger().format("Ontrack DSL script result evaluated to %s", result);
+        theBuild.setResult(theBuild.getResult().combine(Result.FAILURE));
+        // TODO Environment
+        // End
         return true;
+    }
+
+    private Result toJenkinsResult(Object shellResult) {
+        if (shellResult == null ||
+                shellResult.equals(0) ||
+                shellResult.equals(false) ||
+                shellResult.equals("")) {
+            return Result.SUCCESS;
+        } else {
+            return Result.FAILURE;
+        }
+    }
+
+    private Ontrack createOntrackConnector() {
+        OntrackConfiguration config = OntrackConfiguration.getOntrackConfiguration();
+        OntrackConnection connection = OntrackConnection.create(config.getOntrackUrl());
+        String user = config.getOntrackUser();
+        if (StringUtils.isNotBlank(user)) {
+            connection = connection.authenticate(
+                    user,
+                    config.getOntrackPassword()
+            );
+        }
+        return connection.build();
     }
 
     @Extension
