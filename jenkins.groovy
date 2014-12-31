fda44f103a4f8c8dc29334d8cb69cf88386c750b
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-12-31 16:19:27 +0100

    #23 Jenkins Release (wip)

--- jenkins.groovy
+++ jenkins.groovy
@@ -37,5 +37,71 @@ job {
                 '**/target/**',
                 'FIXME', 'TODO', '@Deprecated', true
         )
+        buildPipelineTrigger("ontrack-jenkins/ontrack-jenkins-release") {
+            parameters {
+                currentBuild()
+            }
+        }
+    }
+}
+
+job {
+    name "ontrack-jenkins/ontrack-jenkins-release"
+    description "Release job for the Ontrack Jenkins plug-in"
+    logRotator(numToKeep = 40)
+    deliveryPipelineConfiguration('Commit', 'CI')
+    parameters {
+        stringParam('VERSION', '', '')
+        stringParam('NEXT_VERSION', '', '')
+    }
+    jdk 'JDK8u20'
+    scm {
+        git {
+            remote {
+                url 'git@github.com:nemerosa/ontrack-jenkins.git'
+                branch "origin/master"
+            }
+            wipeOutWorkspace true
+            localBranch "master"
+        }
+    }
+    wrappers {
+        toolenv('Maven-3.2.x')
+    }
+    steps {
+        shell '''\
+export PATH=${MAVEN_3_2_X_HOME}/bin:$PATH
+mvn versions:set -DgenerateBackupPoms=false -Dversion=${VERSION}
+git commit -am "Release ${VERSION}"
+git tag "${VERSION}"
+'''
+        maven {
+            goals 'clean install'
+            mavenInstallation 'Maven-3.2.x'
+        }
+        shell '''\
+export PATH=${MAVEN_3_2_X_HOME}/bin:$PATH
+mvn versions:set -DgenerateBackupPoms=false -Dversion=${NEXT_VERSION}-SNAPSHOT
+git commit -am "Starting ${NEXT_VERSION}"
+'''
+    }
+    publishers {
+        archiveJunit("**/target/surefire-reports/*.xml")
+        tasks(
+                '**/*.java,**/*.groovy,**/*.xml,**/*.html,**/*.js',
+                '**/target/**',
+                'FIXME', 'TODO', '@Deprecated', true
+        )
+    }
+}
+
+view(type: DeliveryPipelineView) {
+    name "ontrack-jenkins/Pipeline"
+    pipelineInstances(4)
+    enableManualTriggers()
+    showChangeLog()
+    updateInterval(5)
+    pipelines {
+        component("ontrack-jenkins", "ontrack-jenkins/ontrack-jenkins-ci")
     }
 }
