9c16aae378aefd1144310e84f79f8e2f0b6aa826
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2015-01-16 09:25:28 +0100

    #27 Choice parameter - input

--- src/main/java/net/nemerosa/ontrack/jenkins/AbstractOntrackMultipleParameterDefinition.java
+++ src/main/java/net/nemerosa/ontrack/jenkins/AbstractOntrackMultipleParameterDefinition.java
@@ -1,15 +1,38 @@
 package net.nemerosa.ontrack.jenkins;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 public abstract class AbstractOntrackMultipleParameterDefinition extends AbstractOntrackParameterDefinition {
 
-    private final String labelProperty;
-
-    public AbstractOntrackMultipleParameterDefinition(String name, String description, String dsl, String valueProperty, String labelProperty) {
+    public AbstractOntrackMultipleParameterDefinition(String name, String description, String dsl, String valueProperty) {
         super(name, description, dsl, valueProperty);
-        this.labelProperty = labelProperty;
     }
 
-    public String getLabelProperty() {
-        return labelProperty;
+    /**
+     * List of possible values
+     */
+    public List<String> getChoices() {
+        // Runs the DSL
+        Object any = runDSL();
+        // Collection of items
+        Collection<?> items;
+        // If not a collection, converts to it
+        if (any instanceof Collection) {
+            items = (Collection<?>) any;
+        } else {
+            items = Collections.singletonList(any);
+        }
+        // Gets the values
+        List<String> values = new ArrayList<String>();
+        // Collects the values
+        for (Object item : items) {
+            values.add(getProperty(item, getValueProperty()));
+        }
+        // OK
+        return values;
     }
+
 }
