4210a78bb2bbb11480d89d7282fdc3cf7f1f55e2
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2016-02-09 08:46:27 +0100

    Restoring DSL extensions

---
+++ src/main/java/net/nemerosa/ontrack/jenkins/extension/OntrackStepContextExtensionPoint.java
@@ -0,0 +1,27 @@
+package net.nemerosa.ontrack.jenkins.extension;
+
+import hudson.Extension;
+import javaposse.jobdsl.dsl.helpers.step.StepContext;
+import javaposse.jobdsl.plugin.ContextExtensionPoint;
+import javaposse.jobdsl.plugin.DslExtensionMethod;
+import net.nemerosa.ontrack.jenkins.OntrackDSLStep;
+
+@Extension(optional = true)
+public class OntrackStepContextExtensionPoint extends ContextExtensionPoint {
+
+    /**
+     * DSL
+     */
+    @DslExtensionMethod(context = StepContext.class)
+    public OntrackDSLStep ontrackDsl(Runnable closure) {
+        OntrackDslContext context = new OntrackDslContext();
+        executeInContext(closure, context);
+        return new OntrackDSLStep(
+                context.getScriptLocation(),
+                context.getInjectEnvironment(),
+                context.getInjectProperties(),
+                context.isLog()
+        );
+    }
+
+}
