33ccb1bb55cc19982416a0276df0147a1bb212f5
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2017-04-24 17:40:51 +0200

    #19 Build creation step - test for nulls

--- src/test/java/net/nemerosa/ontrack/jenkins/steps/OntrackBuildStepRunTest.java
+++ src/test/java/net/nemerosa/ontrack/jenkins/steps/OntrackBuildStepRunTest.java
@@ -4,6 +4,7 @@ import hudson.model.Result;
 import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
 import org.jenkinsci.plugins.workflow.job.WorkflowJob;
 import org.jenkinsci.plugins.workflow.job.WorkflowRun;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
@@ -21,6 +22,37 @@ public class OntrackBuildStepRunTest {
 
         WorkflowRun run = jenkinsRule.assertBuildStatus(Result.FAILURE, job.scheduleBuild2(0).get());
         jenkinsRule.assertLogContains("Argument for @Nonnull parameter 'project' of net/nemerosa/ontrack/jenkins/steps/OntrackBuildStep.<init> must not be null", run);
+    }
+
+    @Test
+    public void test_missing_branch() throws Exception {
+        WorkflowJob job = jenkinsRule.jenkins.createProject(WorkflowJob.class, "workflow");
+        // leave out the subject
+        job.setDefinition(new CpsFlowDefinition("ontrackBuild(project: 'proj', build: '1')", true));
+
+        WorkflowRun run = jenkinsRule.assertBuildStatus(Result.FAILURE, job.scheduleBuild2(0).get());
+        jenkinsRule.assertLogContains("Argument for @Nonnull parameter 'branch' of net/nemerosa/ontrack/jenkins/steps/OntrackBuildStep.<init> must not be null", run);
+    }
+
+    @Test
+    public void test_missing_build() throws Exception {
+        WorkflowJob job = jenkinsRule.jenkins.createProject(WorkflowJob.class, "workflow");
+        // leave out the subject
+        job.setDefinition(new CpsFlowDefinition("ontrackBuild(project: 'proj', branch: 'master')", true));
+
+        WorkflowRun run = jenkinsRule.assertBuildStatus(Result.FAILURE, job.scheduleBuild2(0).get());
+        jenkinsRule.assertLogContains("Argument for @Nonnull parameter 'build' of net/nemerosa/ontrack/jenkins/steps/OntrackBuildStep.<init> must not be null", run);
+    }
+
+    @Test
+    @Ignore
+    public void test_blank_project() throws Exception {
+        WorkflowJob job = jenkinsRule.jenkins.createProject(WorkflowJob.class, "workflow");
+        // leave out the subject
+        job.setDefinition(new CpsFlowDefinition("ontrackBuild(project: '', branch: 'master', build: '1')", true));
+
+        WorkflowRun run = jenkinsRule.assertBuildStatus(Result.FAILURE, job.scheduleBuild2(0).get());
+        jenkinsRule.assertLogContains("Ontrack build not created. All mandatory properties must be supplied ('project', 'branch', 'build').", run);
     }
 
 }
