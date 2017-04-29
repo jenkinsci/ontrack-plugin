48d3f75c3e8a55eba1b02aa8b09a805081631351
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2017-04-29 17:00:25 +0200

    #19 Build creation step - using non deprecated elements

--- src/main/java/net/nemerosa/ontrack/jenkins/steps/OntrackBuildStep.java
+++ src/main/java/net/nemerosa/ontrack/jenkins/steps/OntrackBuildStep.java
@@ -1,5 +1,6 @@
 package net.nemerosa.ontrack.jenkins.steps;
 
+import com.google.common.collect.ImmutableSet;
 import hudson.AbortException;
 import hudson.Extension;
 import hudson.model.TaskListener;
@@ -7,20 +8,17 @@ import net.nemerosa.ontrack.dsl.Branch;
 import net.nemerosa.ontrack.dsl.Ontrack;
 import net.nemerosa.ontrack.jenkins.dsl.OntrackDSLConnector;
 import org.apache.commons.lang.StringUtils;
-import org.jenkinsci.plugins.workflow.steps.AbstractStepDescriptorImpl;
-import org.jenkinsci.plugins.workflow.steps.AbstractStepExecutionImpl;
-import org.jenkinsci.plugins.workflow.steps.AbstractStepImpl;
-import org.jenkinsci.plugins.workflow.steps.StepContext;
+import org.jenkinsci.plugins.workflow.steps.*;
 import org.kohsuke.stapler.DataBoundConstructor;
 
 import javax.annotation.Nonnull;
-import javax.inject.Inject;
+import java.util.Set;
 
 /**
  * Steps to create a build
  */
 @SuppressWarnings("unused")
-public class OntrackBuildStep extends AbstractStepImpl {
+public class OntrackBuildStep extends Step {
 
     /**
      * Name of the project to create the build for
@@ -54,11 +52,36 @@ public class OntrackBuildStep extends AbstractStepImpl {
         return build;
     }
 
-    @Extension
-    public static class DescriptorImpl extends AbstractStepDescriptorImpl {
+    @Override
+    public StepExecution start(final StepContext context) throws Exception {
+        // Checks
+        if (StringUtils.isBlank(this.project) || StringUtils.isBlank(this.branch) || StringUtils.isBlank(this.build)) {
+            throw new AbortException("Ontrack build not created. All mandatory properties must be supplied ('project', 'branch', 'build').");
+        }
+        // Build description
+        final String buildDescription = String.format("Build %s", this.build);
+        // OK
+        return new SynchronousStepExecution<Void>(context) {
+            @Override
+            protected Void run() throws Exception {
+                // Gets the Ontrack connector
+                Ontrack ontrack = OntrackDSLConnector.createOntrackConnector(context.get(TaskListener.class));
+                // Gets the branch...
+                Branch ontrackBranch = ontrack.branch(project, branch);
+                // ... and creates a build
+                ontrackBranch.build(build, buildDescription, true);
+                // Done
+                return null;
+            }
+        };
+    }
 
-        public DescriptorImpl() {
-            super(Execution.class);
+    @Extension
+    public static class DescriptorImpl extends StepDescriptor {
+
+        @Override
+        public Set<? extends Class<?>> getRequiredContext() {
+            return ImmutableSet.of(TaskListener.class);
         }
 
         @Override
@@ -73,42 +96,4 @@ public class OntrackBuildStep extends AbstractStepImpl {
         }
     }
 
-    private static class Execution extends AbstractStepExecutionImpl {
-
-        private final OntrackBuildStep step;
-
-        @Inject
-        public Execution(StepContext stepContext, OntrackBuildStep step) {
-            super(stepContext);
-            this.step = step;
-        }
-
-        @Override
-        public boolean start() throws Exception {
-            // Checks
-            if (StringUtils.isBlank(step.project) || StringUtils.isBlank(step.branch) || StringUtils.isBlank(step.build)) {
-                throw new AbortException("Ontrack build not created. All mandatory properties must be supplied ('project', 'branch', 'build').");
-            }
-            // Expansion
-            String projectName = step.getProject();
-            @SuppressWarnings("ConstantConditions")
-            String branchName = step.getBranch();
-            @SuppressWarnings("ConstantConditions")
-            String buildName = step.getBuild();
-            // Build description
-            String buildDescription = String.format("Build %s", buildName);
-            // Gets the Ontrack connector
-            Ontrack ontrack = OntrackDSLConnector.createOntrackConnector(getContext().get(TaskListener.class));
-            // Gets the branch...
-            Branch branch = ontrack.branch(projectName, branchName);
-            // ... and creates a build
-            branch.build(buildName, buildDescription, true);
-            // OK
-            return true;
-        }
-
-        @Override
-        public void stop(@Nonnull Throwable cause) throws Exception {
-        }
-    }
 }
