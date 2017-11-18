117b32e5fd2ae4ace083c68902374ac6eb25483b
David C.A. Danneels <daviddafke@msn.com>
2017-11-18 02:05:40 +0100

    SFAAS-1581: add/refactor tests

--- src/test/java/net/nemerosa/ontrack/jenkins/OntrackTriggerTest.java
+++ src/test/java/net/nemerosa/ontrack/jenkins/OntrackTriggerTest.java
@@ -6,28 +6,35 @@ import org.junit.Test;
 
 public class OntrackTriggerTest {
 
+    public static final String VALID_SPEC = "H H(2-5) * * *";
+    public static final String INVALID_SPEC = "H H(2-5)";
+    public static final String PROJECT = "project";
+    public static final String BRANCH = "branch";
+    public static final String PROMOTION = "promotion";
+    public static final String PARAMETER_NAME = "parameterName";
 
     @Test(expected = ANTLRException.class)
     public void invalid_cron_expression_results_in_exception() throws ANTLRException{
         OntrackTrigger trigger = new OntrackTrigger(
-                "H H(2-5)",
-                "project",
-                "branch",
-                "promotion",
-                "parameterName",
+                INVALID_SPEC,
+                PROJECT,
+                BRANCH,
+                PROMOTION,
+                PARAMETER_NAME,
                 null
         );
     }
-      @Test
-    public void default_result_is_success() {
+
+    @Test
+    public void default_result_is_success_for_null() {
         OntrackTrigger trigger = null;
         try {
             trigger = new OntrackTrigger(
-                    "H H(2-5) * * *",
-                    "project",
-                    "branch",
-                    "promotion",
-                    "parameterName",
+                    VALID_SPEC,
+                    PROJECT,
+                    BRANCH,
+                    PROMOTION,
+                    PARAMETER_NAME,
                     null
             );
         } catch (ANTLRException e) {
@@ -37,17 +44,35 @@ public class OntrackTriggerTest {
         assert trigger.getMinimumResult() == Result.SUCCESS;
     }
 
+    @Test
+    public void default_result_is_success_for_empty() {
+        OntrackTrigger trigger = null;
+        try {
+            trigger = new OntrackTrigger(
+                    VALID_SPEC,
+                    PROJECT,
+                    BRANCH,
+                    PROMOTION,
+                    PARAMETER_NAME,
+                    ""
+            );
+        } catch (ANTLRException e) {
+
+            e.printStackTrace();
+        }
+        assert trigger.getMinimumResult() == Result.SUCCESS;
+    }
 
     @Test
     public void invalid_result_becomes_failure() {
         OntrackTrigger trigger = null;
         try {
             trigger = new OntrackTrigger(
-                    "H H(2-5) * * *",
-                    "project",
-                    "branch",
-                    "promotion",
-                    "parameterName",
+                    VALID_SPEC,
+                    PROJECT,
+                    BRANCH,
+                    PROMOTION,
+                    PARAMETER_NAME,
                     "BROL"
             );
         } catch (ANTLRException e) {
@@ -62,11 +87,11 @@ public class OntrackTriggerTest {
         OntrackTrigger trigger = null;
         try {
             trigger = new OntrackTrigger(
-                    "H H(2-5) * * *",
-                    "project",
-                    "branch",
-                    "promotion",
-                    "parameterName",
+                    VALID_SPEC,
+                    PROJECT,
+                    BRANCH,
+                    PROMOTION,
+                    PARAMETER_NAME,
                     "UNSTABLE"
             );
         } catch (ANTLRException e) {
--- src/test/java/net/nemerosa/ontrack/jenkins/extension/OntrackTriggerContextExtensionPointTest.java
+++ src/test/java/net/nemerosa/ontrack/jenkins/extension/OntrackTriggerContextExtensionPointTest.java
@@ -7,22 +7,105 @@ import org.junit.Test;
 
 public class OntrackTriggerContextExtensionPointTest {
 
+    public static final String VALID_SPEC = "H H(2-5) * * *";
+    public static final String INVALID_SPEC = "H H(2-5)";
+    public static final String PROJECT = "project";
+    public static final String BRANCH = "branch";
+    public static final String PROMOTION = "promotion";
+    public static final String PARAMETER_NAME = "parameterName";
+
     @Test
-    public void test(){
+    public void set_custom_minimum_result(){
         OntrackTriggerContextExtensionPoint extensionPoint = new OntrackTriggerContextExtensionPoint();
         OntrackTrigger trigger = null;
         try {
             trigger = extensionPoint.ontrackTrigger(
-                    "H H(2-5) * * *",
-                    "project",
-                    "name",
-                    "promotion",
-                    "parameterName",
+                    VALID_SPEC,
+                    PROJECT,
+                    BRANCH,
+                    PROMOTION,
+                    PARAMETER_NAME,
                     "UNSTABLE"
             );
         } catch (ANTLRException e) {
             e.printStackTrace();
         }
         assert trigger.getMinimumResult() == Result.UNSTABLE;
+    }
+
+
+    @Test
+    public void invalid_value_results_in_failure(){
+        OntrackTriggerContextExtensionPoint extensionPoint = new OntrackTriggerContextExtensionPoint();
+        OntrackTrigger trigger = null;
+        try {
+            trigger = extensionPoint.ontrackTrigger(
+                    VALID_SPEC,
+                    PROJECT,
+                    BRANCH,
+                    PROMOTION,
+                    PARAMETER_NAME,
+                    "BROL"
+            );
+        } catch (ANTLRException e) {
+            e.printStackTrace();
+        }
+        assert trigger.getMinimumResult() == Result.FAILURE;
+    }
+
+    @Test
+    public void default_value_is_success_for_null(){
+        OntrackTriggerContextExtensionPoint extensionPoint = new OntrackTriggerContextExtensionPoint();
+        OntrackTrigger trigger = null;
+        try {
+            trigger = extensionPoint.ontrackTrigger(
+                    VALID_SPEC,
+                    PROJECT,
+                    BRANCH,
+                    PROMOTION,
+                    PARAMETER_NAME,
+                    null
+            );
+        } catch (ANTLRException e) {
+            e.printStackTrace();
+        }
+        assert trigger.getMinimumResult() == Result.SUCCESS;
+    }
+
+    @Test
+    public void default_value_is_success_for_empty(){
+        OntrackTriggerContextExtensionPoint extensionPoint = new OntrackTriggerContextExtensionPoint();
+        OntrackTrigger trigger = null;
+        try {
+            trigger = extensionPoint.ontrackTrigger(
+                    VALID_SPEC,
+                    PROJECT,
+                    BRANCH,
+                    PROMOTION,
+                    PARAMETER_NAME,
+                    ""
+            );
+        } catch (ANTLRException e) {
+            e.printStackTrace();
+        }
+        assert trigger.getMinimumResult() == Result.SUCCESS;
+    }
+
+    @Test
+    public void default_value_is_success_if_missing_parameter(){
+        OntrackTriggerContextExtensionPoint extensionPoint = new OntrackTriggerContextExtensionPoint();
+        OntrackTrigger trigger = null;
+        try {
+            trigger = extensionPoint.ontrackTrigger(
+                    VALID_SPEC,
+                    PROJECT,
+                    BRANCH,
+                    PROMOTION,
+                    PARAMETER_NAME
+            );
+        } catch (ANTLRException e) {
+            e.printStackTrace();
+        }
+        assert trigger.getMinimumResult() == Result.SUCCESS;
     }
 }
