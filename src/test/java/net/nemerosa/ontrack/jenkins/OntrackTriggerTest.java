e9a75a21baa700acb41fd672efca04f0689f82f9
David C.A. Danneels <daviddafke@msn.com>
2017-11-18 01:34:44 +0100

    SFAAS-1581: make minimumResult configurable (iso SUCCESS) + add tests

---
+++ src/test/java/net/nemerosa/ontrack/jenkins/OntrackTriggerTest.java
@@ -0,0 +1,78 @@
+package net.nemerosa.ontrack.jenkins;
+
+import antlr.ANTLRException;
+import hudson.model.Result;
+import org.junit.Test;
+
+public class OntrackTriggerTest {
+
+
+    @Test(expected = ANTLRException.class)
+    public void invalid_cron_expression_results_in_exception() throws ANTLRException{
+        OntrackTrigger trigger = new OntrackTrigger(
+                "H H(2-5)",
+                "project",
+                "branch",
+                "promotion",
+                "parameterName",
+                null
+        );
+    }
+      @Test
+    public void default_result_is_success() {
+        OntrackTrigger trigger = null;
+        try {
+            trigger = new OntrackTrigger(
+                    "H H(2-5) * * *",
+                    "project",
+                    "branch",
+                    "promotion",
+                    "parameterName",
+                    null
+            );
+        } catch (ANTLRException e) {
+
+            e.printStackTrace();
+        }
+        assert trigger.getMinimumResult() == Result.SUCCESS;
+    }
+
+
+    @Test
+    public void invalid_result_becomes_failure() {
+        OntrackTrigger trigger = null;
+        try {
+            trigger = new OntrackTrigger(
+                    "H H(2-5) * * *",
+                    "project",
+                    "branch",
+                    "promotion",
+                    "parameterName",
+                    "BROL"
+            );
+        } catch (ANTLRException e) {
+
+            e.printStackTrace();
+        }
+        assert trigger.getMinimumResult() == Result.FAILURE;
+    }
+
+    @Test
+    public void valid_result_is_used() {
+        OntrackTrigger trigger = null;
+        try {
+            trigger = new OntrackTrigger(
+                    "H H(2-5) * * *",
+                    "project",
+                    "branch",
+                    "promotion",
+                    "parameterName",
+                    "UNSTABLE"
+            );
+        } catch (ANTLRException e) {
+
+            e.printStackTrace();
+        }
+        assert trigger.getMinimumResult() == Result.UNSTABLE;
+    }
+}
