1b2dc0a2fdbf37b72022c0835c461cb9ce290c9c
David C.A. Danneels <daviddafke@msn.com>
2017-12-13 11:34:13 +0100

    add separate class for multiChoiceParameters

---
+++ src/main/java/net/nemerosa/ontrack/jenkins/OntrackMultiChoiceParameterDefinition.java
@@ -0,0 +1,69 @@
+package net.nemerosa.ontrack.jenkins;
+
+import hudson.Extension;
+import hudson.model.ParameterValue;
+import hudson.model.StringParameterValue;
+import net.sf.json.JSONObject;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.StaplerRequest;
+
+import java.util.List;
+
+public class OntrackMultiChoiceParameterDefinition extends AbstractOntrackMultipleParameterDefinition {
+
+    @DataBoundConstructor
+    public OntrackMultiChoiceParameterDefinition(String name, String description, String dsl, String valueProperty) {
+        super(name, description, dsl, valueProperty);
+    }
+
+    @Override
+    public ParameterValue createValue(StaplerRequest req, JSONObject jo) {
+        StringParameterValue value = req.bindJSON(StringParameterValue.class, jo);
+        value.setDescription(getDescription());
+        return value;
+    }
+
+    @Override
+    public ParameterValue createValue(StaplerRequest req) {
+        String[] value = req.getParameterValues(getName());
+        if (value == null) {
+            return getDefaultParameterValue();
+        } else if (value.length != 1) {
+            throw new IllegalArgumentException(String.format(
+                    "Illegal number of parameter values for %s: %d",
+                    getName(),
+                    value.length));
+        } else {
+            List<String> choices = getChoices();
+            if (choices.contains(value[0])) {
+                return new StringParameterValue(getName(), value[0], getDescription());
+            } else {
+                throw new IllegalArgumentException(String.format(
+                        "Value %s for parameter %s is not a valid choice.",
+                        value[0],
+                        getName()));
+            }
+        }
+    }
+
+    @Override
+    public StringParameterValue getDefaultParameterValue() {
+        List<String> choices = getChoices();
+        String value = choices.isEmpty() ? "" : choices.get(0);
+        return new StringParameterValue(getName(), value, getDescription());
+    }
+
+    @Extension
+    public static class DescriptorImpl extends ParameterDescriptor {
+
+        @Override
+        public String getDisplayName() {
+            return "Ontrack: Multi Parameter choice";
+        }
+
+        @Override
+        public String getHelpFile() {
+            return "/help/ontrack/parameter-choice.html";
+        }
+    }
+}
